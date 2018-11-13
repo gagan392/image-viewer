121f586bc38a6fcb8cc6da95b5186f23f01e23e8
Gagan Raj M <gagan.rajm@ellucian.com>
2018-11-14 04:10:54 +0530

    Adding Header and Login component

---
+++ src/common/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+	return (
+		<div className="app-header">
+			<div className="app-logo">Image Viewer</div>
+		</div>
+	)
+}
+
+export default Header;
