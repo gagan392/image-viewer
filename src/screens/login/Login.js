4059806904a025e0ecc194ea74a8407fc256b981
Gagan Raj M <gagan.rajm@ellucian.com>
2018-11-14 06:14:17 +0530

    Added login card with validations, error messages, hanlder

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,16 +1,105 @@
 import React, { Component } from 'react';
 import Header from './../../common/header/Header';
 
+import { Card, CardContent } from "@material-ui/core";
+import { FormControl, InputLabel, Input, Button, FormHelperText } from "@material-ui/core";
+import { Typography, withStyles } from "@material-ui/core";
+
 import './Login.css';
 
+const styles = () => ({
+	contentRoot: {
+		'padding': '60px !important'
+	},
+	formControlRoot: {
+		'width': '-webkit-fill-available'
+	}
+})
 class Login extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: '',
+			usernameReqdClass: 'dispNone',
+			password: '',
+			passwordReqdClass: 'dispNone',
+			credentialsIncorrectClass: 'dispNone'
+		}
+	}
+
+	usernameChangeHandler = e => {
+		this.setState({
+			username: e.target.value
+		});
+	}
+
+	passwordChangeHandler = e => {
+		this.setState({
+			password: e.target.value
+		});
+	}
+
+	loginButtonClickHandler = () => {
+
+		const username = 'gagan', password = '1234asdf', accessToken = '3302157035.3d32df8.313259d349224c14927b36bc7b697203Î', currState = this.state;
+		currState.usernameReqdClass = this.state.username === "" ? "dispBlock" : "dispNone";
+		currState.passwordReqdClass = this.state.password === "" ? "dispBlock" : "dispNone";
+		if (this.state.username && this.state.password) {
+			currState.credentialsIncorrectClass = this.state.username !== username || this.state.password !== password ? "dispBlock" : "dispNone";
+		}
+		this.setState(currState);
+
+
+		if (!this.state.username || !this.state.password) {
+			return;
+		}
+
+		if (this.state.username !== username || this.state.password !== password) {
+			return;
+		}
+
+		sessionStorage.setItem("access-token", accessToken);
+
+	}
+
 	render() {
+		const { classes } = this.props;
 		return (
-			<div>
+			<>
 				<Header />
-			</div>
+				<Card className="cardStyle">
+					<CardContent classes={{ root: classes.contentRoot }}>
+						<Typography variant="headline" component="h2"> LOGIN </Typography><br />
+						<FormControl classes={{ root: classes.formControlRoot }} required>
+							<InputLabel htmlFor="username">Username</InputLabel>
+							<Input id="username" username={this.state.username} type="string" onChange={this.usernameChangeHandler} />
+							<FormHelperText className={this.state.usernameReqdClass}>
+								<span className="textColorRed">required</span>
+							</FormHelperText>
+						</FormControl>
+						<br /><br />
+
+						<FormControl classes={{ root: classes.formControlRoot }} required>
+							<InputLabel htmlFor="password">Password</InputLabel>
+							<Input id="password" password={this.state.password} type="password" onChange={this.passwordChangeHandler} />
+							<FormHelperText className={this.state.passwordReqdClass}>
+								<span className="textColorRed">required</span>
+							</FormHelperText>
+						</FormControl>
+						<br /><br />
+
+						<div className={this.state.credentialsIncorrectClass}>
+							<span className="textColorRed">Incorrect username and/or password</span>
+							<br /><br />
+						</div>
+
+						<Button variant="contained" color="primary" onClick={this.loginButtonClickHandler}> Login </Button>
+					</CardContent>
+				</Card>
+			</>
 		)
 	}
 }
 
-export default Login;
+export default withStyles(styles)(Login);
